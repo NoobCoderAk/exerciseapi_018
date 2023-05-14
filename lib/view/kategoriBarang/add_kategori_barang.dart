8a66a42a1ad6b6ab9926e95eb2fdf7ddb60074ea
NoobCoderAk <abidakbar0001@gmail.com>
2023-05-14 12:24:38 +0700

    create object and variable

--- lib/view/kategoriBarang/add_kategori_barang.dart
+++ lib/view/kategoriBarang/add_kategori_barang.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:kategori_app/controller/kategori_barang_controller.dart';
 
 class AddKategoriBarang extends StatefulWidget {
   const AddKategoriBarang({super.key});
@@ -9,6 +10,10 @@ class AddKategoriBarang extends StatefulWidget {
 }
 
 class _AddKategoriBarangState extends State<AddKategoriBarang> {
+  //membuta object kategoriBarangController dari class KategoriBarangController()
+  final kategoriBarangController = KategoriBarangController();
+  String? nama;
+
   @override
   Widget build(BuildContext context) {
     return Container();
