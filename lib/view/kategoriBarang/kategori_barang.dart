846920c50bea97cb595d45f278a9506a394ba4c1
NoobCoderAk <abidakbar0001@gmail.com>
2023-05-14 13:00:15 +0700

    create statefulwidget on kategori_barang.dart

--- lib/view/kategoriBarang/kategori_barang.dart
+++ lib/view/kategoriBarang/kategori_barang.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
