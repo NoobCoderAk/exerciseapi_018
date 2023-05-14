d68ec6c524ca5e7d217542b85c7c544bd7373bda
NoobCoderAk <abidakbar0001@gmail.com>
2023-05-14 15:58:23 +0700

    create update_kategori_barang.dart

---
+++ lib/view/kategoriBarang/update_kategori_barang.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UpdateKategoriBarang extends StatefulWidget {
+  const UpdateKategoriBarang({super.key});
+
+  @override
+  State<UpdateKategoriBarang> createState() => _UpdateKategoriBarangState();
+}
+
+class _UpdateKategoriBarangState extends State<UpdateKategoriBarang> {
+  @override
+  Widget build(BuildContext context) {
+    var fromkey = GlobalKey<FormState>();
+
+    return Scaffold();
+  }
+}
