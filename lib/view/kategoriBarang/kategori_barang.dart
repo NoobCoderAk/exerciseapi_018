070d82fae07408b9774e1e739ac00d909020441b
NoobCoderAk <abidakbar0001@gmail.com>
2023-05-14 13:13:01 +0700

    create object and variable on kategori_barang.dart

--- lib/view/kategoriBarang/kategori_barang.dart
+++ lib/view/kategoriBarang/kategori_barang.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:kategori_app/controller/kategori_barang_controller.dart';
+import 'package:kategori_app/model/kategori_barang_model.dart';
 
 class KategoriBarang extends StatefulWidget {
   const KategoriBarang({super.key});
@@ -9,6 +11,12 @@ class KategoriBarang extends StatefulWidget {
 }
 
 class _KategoriBarangState extends State<KategoriBarang> {
+  // membuat object kategoriBarangController dari class kategoriBarangController()
+  final kategoriBarangController = KategoriBarangController();
+  // membuat List listKategoriBarang dari class KategoriBarangModel
+  List<KategoriBarangModel> listKategoriBarang = [];
+  // membuat variable nama tipe String
+  String? nama;
   @override
   Widget build(BuildContext context) {
     return Container();
